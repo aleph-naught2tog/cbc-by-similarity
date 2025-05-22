6c5a4004fc4df9e805af0272de7476959f4bed76
Max Cerrina <mrcerrina@gmail.com>
2025-05-22 07:11:11 -0500

    wip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
 import json
 import os
 from typing import Tuple, NewType, cast
+import numpy as np
 
 # in:  { [birdName]: { year: { howMany, numberByPartyHours }}}
 # out: bird_name,year,how_many,number_by_party_hours
@@ -19,6 +21,7 @@ def translate_bird_map_json_to_csv(
     input_filename: str, output_filename: str
 ) -> None:
     if os.path.exists(output_filename):
+        print("Output file exists; no need to recreate.")
         return
 
     csv_data: BirdDataCSV = cast(BirdDataCSV, [])
@@ -26,53 +29,31 @@ def translate_bird_map_json_to_csv(
 
     csv_data.append(header)
 
-    with open(input_filename) as f:
+    with open(input_filename, newline="") as f:
         loaded_json = json.load(f)
         for bird_name, results_by_year in loaded_json.items():
             for year, data in results_by_year.items():
                 row = [
                     bird_name,
-                    year,
+                    (year),
                     data["howMany"],
                     data["numberByPartyHours"],
                 ]
                 csv_data.append(row)
 
-    write_bird_csv(csv_data=csv_data, filename=output_filename)
-
-
-def write_bird_csv(
-    csv_data: BirdDataCSV, filename: str = "data/processed/bird_map.csv"
-) -> None:
-    with open(filename, "w") as f:
-        for row in csv_data:
-            row_length = len(row)
-            for index, value in enumerate(row):
-                value_string = ""
-
-                if type(value) is str:
-                    value_string = f'"{value}"'
-                elif type(value) is int or type(value) is float:
-                    value_string = str(value)
-                elif value is None:
-                    value_string = ""
-                else:
-                    raise Exception(f"Unexpected type, {value}, {type(value)}")
-
-                # only add trailing comma if it's not the last item
-                if index < row_length - 1:
-                    value_string += ','
-
-                f.write(value_string)
-            f.write('\n')
+    with open(output_filename, "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_STRINGS)
+        # I don't understand why the years are being quoted but that's fine
+        writer.writerows(csv_data)
 
 
 def main():
     input_filename = "data/raw/bird_map_as_json.json"
     output_filename = "data/processed/bird_map.csv"
-    translate_bird_map_json_to_csv(
-        input_filename=input_filename, output_filename=output_filename
-    )
+    # I feel like there might be a more built-in way of doing this, or skipping the CSV write step completely, but fine for now
+    translate_bird_map_json_to_csv(input_filename, output_filename)
+
+    res = np.genfromtext(output_filename, delimiter=',')
 
 
 if __name__ == "__main__":
