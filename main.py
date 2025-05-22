bab8c434e5568d969b3923e0a5361d78ff57a1cf
Max Cerrina <mrcerrina@gmail.com>
2025-05-22 06:37:14 -0500

    bump python version, add types, translate to csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Tuple, NewType, cast
 
 # in:  { [birdName]: { year: { howMany, numberByPartyHours }}}
 # out: bird_name,year,how_many,number_by_party_hours
@@ -8,12 +9,19 @@ import os
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.directed_hausdorff.html
 # array
 
+BirdDataCSV = NewType(
+    "BirdDataCSV",
+    list[list[str] | Tuple[str, int, float | None, float | None]],
+)
 
-def translate_bird_map_json_to_csv(input_filename: str, output_filename: str) -> None:
+
+def translate_bird_map_json_to_csv(
+    input_filename: str, output_filename: str
+) -> None:
     if os.path.exists(output_filename):
         return
 
-    csv_data: list[list[str] | list[(str, int, float | None, float | None)]] = []
+    csv_data: BirdDataCSV = cast(BirdDataCSV, [])
     header = ["bird_name", "year", "how_many", "number_by_party_hours"]
 
     csv_data.append(header)
@@ -22,32 +30,41 @@ def translate_bird_map_json_to_csv(input_filename: str, output_filename: str) ->
         loaded_json = json.load(f)
         for bird_name, results_by_year in loaded_json.items():
             for year, data in results_by_year.items():
-                row = [bird_name, year, data["howMany"], data["numberByPartyHours"]]
+                row = [
+                    bird_name,
+                    year,
+                    data["howMany"],
+                    data["numberByPartyHours"],
+                ]
                 csv_data.append(row)
 
     write_bird_csv(csv_data=csv_data, filename=output_filename)
 
 
 def write_bird_csv(
-    csv_data: list[list[str]], filename: str = "data/processed/bird_map.csv"
+    csv_data: BirdDataCSV, filename: str = "data/processed/bird_map.csv"
 ) -> None:
     with open(filename, "w") as f:
         for row in csv_data:
-            for value in row:
-                value_string = ''
+            row_length = len(row)
+            for index, value in enumerate(row):
+                value_string = ""
 
                 if type(value) is str:
                     value_string = f'"{value}"'
                 elif type(value) is int or type(value) is float:
-                    value_string = value
-                elif type(value) is None:
-                    value_string = ''
+                    value_string = str(value)
+                elif value is None:
+                    value_string = ""
                 else:
-                    raise Exception(f"Unexpected value type, {value}, {type(value)}")
+                    raise Exception(f"Unexpected type, {value}, {type(value)}")
 
-                # value_string += ','
-                # f.write(value_string)
-            # f.write('\n')
+                # only add trailing comma if it's not the last item
+                if index < row_length - 1:
+                    value_string += ','
+
+                f.write(value_string)
+            f.write('\n')
 
 
 def main():
