c56d705e40925add2e6cc447e5cc17662ba82724
Max Cerrina <mrcerrina@gmail.com>
2025-05-25 08:27:03 -0500

    Make a simple class for data handling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
 import json
-import os
-from typing import Tuple, NewType, cast
+from typing import NewType, TypedDict, cast
 import numpy as np
 
 # in:  { [birdName]: { year: { howMany, numberByPartyHours }}}
@@ -11,49 +9,73 @@ import numpy as np
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.directed_hausdorff.html
 # array
 
-BirdDataCSV = NewType(
-    "BirdDataCSV",
-    list[list[str] | Tuple[str, int, float | None, float | None]],
+RawCountDatum = TypedDict(
+    "RawCountDatum",
+    {"howMany": float | None, "numberByPartyHours": float | None},
+)
+CountDatum = TypedDict(
+    "CountDatum",
+    {"how_many": float | None, "number_by_party_hours": float | None},
 )
 
 
-def translate_bird_map_json_to_csv(
-    input_filename: str, output_filename: str
-) -> None:
-    if os.path.exists(output_filename):
-        print("Output file exists; no need to recreate.")
-        return
+BirdJSON = NewType("BirdJSON", dict[str, dict[str, RawCountDatum]])
 
-    csv_data: BirdDataCSV = cast(BirdDataCSV, [])
-    header = ["bird_name", "year", "how_many", "number_by_party_hours"]
 
-    csv_data.append(header)
+class BirdData:
+    def __init__(self, json: BirdJSON) -> None:
+        self.__raw_json__ = json
+
+        self.bird_names = list(self.__raw_json__)
+        self.years = list(self.__raw_json__[self.bird_names[0]])
+
+    def get_by_bird(self, bird_name: str) -> dict[str, RawCountDatum]:
+        if bird_name in self.bird_names:
+            return self.__raw_json__[bird_name]
+        else:
+            raise Exception(f"<{bird_name}> not found in data")
+
+    def get_counts_by_bird(
+        self, bird_name: str
+    ) -> list[tuple[str, float | None]]:
+        bird_data = self.get_by_bird(bird_name)
+
+        counts = [
+            (year, datum["howMany"]) for [year, datum] in bird_data.items()
+        ]
+
+        return counts
+
+    def get_party_hours_by_bird(
+        self, bird_name: str
+    ) -> list[tuple[str, float | None]]:
+        bird_data = self.get_by_bird(bird_name)
+
+        party_hours = [
+            (year, datum["numberByPartyHours"])
+            for [year, datum] in bird_data.items()
+        ]
+
+        return party_hours
+
+
+def translate_json_to_bird_data(input_filename: str) -> BirdData:
+    raw_json = {}
 
     with open(input_filename, newline="") as f:
-        loaded_json = json.load(f)
-        for bird_name, results_by_year in loaded_json.items():
-            for year, data in results_by_year.items():
-                row = [
-                    bird_name,
-                    (year),
-                    data["howMany"],
-                    data["numberByPartyHours"],
-                ]
-                csv_data.append(row)
+        raw_json = json.load(f)
 
-    with open(output_filename, "w", newline="") as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_STRINGS)
-        # I don't understand why the years are being quoted but that's fine
-        writer.writerows(csv_data)
+    bird_data = BirdData(raw_json)
+
+    return bird_data
 
 
 def main():
     input_filename = "data/raw/bird_map_as_json.json"
-    output_filename = "data/processed/bird_map.csv"
-    # I feel like there might be a more built-in way of doing this, or skipping the CSV write step completely, but fine for now
-    translate_bird_map_json_to_csv(input_filename, output_filename)
 
-    res = np.genfromtext(output_filename, delimiter=',')
+    bird_data = translate_json_to_bird_data(input_filename)
+
+    print(bird_data.get_by_bird("Greater White-fronted Goose"))
 
 
 if __name__ == "__main__":
