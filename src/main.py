15723a362273381a10a3e3fe7beba533c403804c
Max Cerrina <mrcerrina@gmail.com>
2025-05-26 07:25:37 -0500

    start playing with kmeans

--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ import numpy as np
 from scipy.spatial.distance import cosine, directed_hausdorff
 from typing import Any, Literal, Sequence
 
-from BirdData import BirdData
+import matplotlib.pyplot as plt
 
-np.seterr(all="warn")
+from sklearn.cluster import KMeans
+
+from BirdData import BirdData
 
 
 def translate_json_to_bird_data(input_filename: str) -> BirdData:
@@ -131,6 +133,7 @@ def write_hausdorff_distances_file(
 ):
     header = ["bird_name"] + bird_data.bird_names
     csv_lines: list[Sequence[float | str]] = [header]
+
     for bird_name in bird_data.bird_names:
         results = calculate_hausdorff_distance(
             data=bird_data,
@@ -149,20 +152,111 @@ def write_hausdorff_distances_file(
     write_to_tsv(csv_lines=csv_lines, output_filename=output_filename)
 
 
+def kmeans_test_run(bird_data: BirdData):
+    bird_list = [
+        (year, to_float_with_default(count))
+        for (year, count) in bird_data.get_by_bird("House Sparrow", "howMany")
+    ]
+
+    x = list(list(zip(*bird_list)))[0]
+    y = list(list(zip(*bird_list)))[1]
+
+    # plt.scatter(x, y)
+    # plt.show()
+
+    data = np.array(bird_list)
+    inertias = []
+
+    # the max number of clusters is 1 per point
+    cluster_range = range(1, len(bird_list) + 1)
+
+    # this gives us an "elbow" to show which cluster count is good
+    for i in cluster_range:
+        print(".", end="")
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(data)
+        inertias.append(kmeans.inertia_)
+
+    # plt.plot(cluster_range, inertias, marker="o")
+    # plt.title("Elbow method")
+    # plt.xlabel("Number of clusters")
+    # plt.ylabel("Inertia")
+    # plt.show()
+
+    # 2
+    kmeans = KMeans(n_clusters=3)
+    kmeans.fit(data)
+
+    plt.scatter(x, y, c=kmeans.labels_)
+    plt.show()
+
+def cosine_kmeans(bird_data: BirdData):
+    x_indices = range(0, len(bird_data.bird_names))
+    results_by_bird_index: list[list[float]] = []
+    house_sparrow_index = bird_data.bird_names.index("House Sparrow")
+
+    # get the values
+    for bird_name in bird_data.bird_names:
+        results = calculate_cosine_similarities(
+            data=bird_data,
+            comparison_bird_name=bird_name,
+            how='howMany',
+        )
+
+        vals: list[float] = list(list(zip(*results))[1])
+        results_by_bird_index.append(vals)
+
+    house_sparrow_data = results_by_bird_index[house_sparrow_index]
+
+    x = x_indices
+    y = house_sparrow_data
+
+    data = np.array(list(zip(x, y)))
+    inertias = []
+
+    # the max number of clusters is 1 per point
+    cluster_range = range(1, len(x))
+
+    # this gives us an "elbow" to show which cluster count is good
+    # for i in cluster_range:
+    #     print(".", end="")
+    #     kmeans = KMeans(n_clusters=i)
+    #     kmeans.fit(data)
+    #     inertias.append(kmeans.inertia_)
+
+    # plt.plot(cluster_range, inertias, marker="o")
+    # plt.title("Elbow method")
+    # plt.xlabel("Number of clusters")
+    # plt.ylabel("Inertia")
+    # plt.show()
+
+    cluster_count = 4
+    kmeans = KMeans(n_clusters=cluster_count)
+    kmeans.fit(data)
+
+    print(len(x), len(y))
+
+    plt.scatter(x, y, c=kmeans.labels_)
+    plt.show()
+
+
 def main():
     input_filename = "data/raw/bird_map_as_json.json"
 
     bird_data = translate_json_to_bird_data(input_filename)
 
-    write_cosine_similarities_file(bird_data=bird_data, how="howMany")
-    write_cosine_similarities_file(
-        bird_data=bird_data, how="numberByPartyHours"
-    )
+    # kmeans_test_run(bird_data)
+    cosine_kmeans(bird_data=bird_data)
 
-    write_hausdorff_distances_file(bird_data=bird_data, how="howMany")
-    write_hausdorff_distances_file(
-        bird_data=bird_data, how="numberByPartyHours"
-    )
+    # write_cosine_similarities_file(bird_data=bird_data, how="howMany")
+    # write_cosine_similarities_file(
+    #     bird_data=bird_data, how="numberByPartyHours"
+    # )
+
+    # write_hausdorff_distances_file(bird_data=bird_data, how="howMany")
+    # write_hausdorff_distances_file(
+    #     bird_data=bird_data, how="numberByPartyHours"
+    # )
 
 
 if __name__ == "__main__":
